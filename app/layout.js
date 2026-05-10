a7588315d6693d2d6c4ace8f8ea2e0d2d0101d85
koopindiatech <koopindiatech@gmail.com>
2026-05-10 22:24:25 +0530

    layout update

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,13 @@ import "./globals.css";
 import ClientLayout from "@/components/layout/ClientLayout";
 import ExtrasWrapper from "@/components/layout/ExtrasWrapper";
 import { ModalProvider } from "@/context/ModalContext";
+import Script from "next/script"; 
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Koop India | Business Launchpad for Startups & Entrepreneurs",
@@ -19,8 +23,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-         <noscript>
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];
+    w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+    j.async=true;
+    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+    f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KD68V3ZG');
+  `}
+        </Script>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KD68V3ZG"
             height="0"
@@ -30,10 +51,8 @@ export default function RootLayout({ children }) {
         </noscript>
         <ModalProvider>
           {/* ClientLayout handles Header, Footer and Scroll Modal */}
-          <ClientLayout>
-            {children}
-          </ClientLayout>
-          
+          <ClientLayout>{children}</ClientLayout>
+
           {/* ExtrasWrapper handles pathname-based FloatingContact visibility */}
           <ExtrasWrapper />
         </ModalProvider>
