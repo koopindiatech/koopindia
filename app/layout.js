a1c29142fd0c4aed7cf8bacf7a117cfc7553ab69
koopindiatech <koopindiatech@gmail.com>
2026-05-16 08:06:08 +0530

    layout update

--- app/layout.js
+++ app/layout.js
@@ -23,9 +23,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta
-          http-equiv="Content-Security-Policy"
-          content="script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://connect.facebook.net https://www.google-analytics.com; img-src 'self' https://www.facebook.com https://www.googletagmanager.com;"
+          httpEquiv="Content-Security-Policy"
+          content="script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://connect.facebook.net https://www.google-analytics.com; img-src 'self' data: https://www.facebook.com https://www.googletagmanager.com;"
         />
+        
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
@@ -41,15 +42,15 @@ export default function RootLayout({ children }) {
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1949577529262037');
-fbq('track', 'PageView');
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1949577529262037');
+            fbq('track', 'PageView');
           `}
         </Script>
       </head>
@@ -63,6 +64,17 @@ fbq('track', 'PageView');
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Meta Pixel Noscript */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1949577529262037&ev=PageView&noscript=1"
+            alt="facebook pixel"
           />
         </noscript>
 
